95c7b00521e7fde34802dd8e04b32f4dacd087bb
Gia Huy <21520909@gm.uit.edu.vn>
2023-12-07 22:02:55 +0700

    ft: authenticate user

--- src/components/pages/SignInPage/SignInPage.jsx
+++ src/components/pages/SignInPage/SignInPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SignInPage.css";
+import axios from "axios";
 
 //pages and components
 import pic from "../../../assets/Group.png";
@@ -19,19 +20,29 @@ function SignInPage() {
     setPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    //Performing authentication
 
-    //If authentication is successful
-    navigate("/dashboard");
+    //Performing authentication
+    try {
+      const response = await axios.get("/api/v1/auth/authenticate", {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        navigate("/dashboard");
+        console.log("User authenticated successfully");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="sign-in-page">
       <div className="sign-in-image">
-        <h1 style={{color: "white"}}>ABC SHOP</h1>
+        <h1 style={{ color: "white" }}>ABC SHOP</h1>
         <img src={pic} alt="sign-in-pic" />
       </div>
       <div className="sign-in-fields">
