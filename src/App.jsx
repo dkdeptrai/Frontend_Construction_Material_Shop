6398ee686a0858ec0a8ed9571cafa32bcf22c5a1
Gia Huy <21520909@gm.uit.edu.vn>
2023-12-11 09:22:28 +0700

    ft: employee list and add information for account setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./App.css";
 import { useSelector } from "react-redux";
 
@@ -8,7 +7,8 @@ import Header from "./components/layouts/header/header.jsx";
 import MenuBar from "./components/layouts/menubar/menubar.jsx";
 import DashBoard from "./components/pages/dashBoardPage.jsx";
 import Customers from "./components/pages/customersPage.jsx";
-import Employees from "./components/pages/employeePages.jsx";
+import Employee from "./components/pages/Employee/ListEmployee/Employee.jsx";
+import AddEmployee from "./components/pages/Employee/AddEmployee/AddEmployeePage.jsx";
 import Inventory from "./components/pages/inventoryPage.jsx";
 import Orders from "./components/pages/ordersPage.jsx";
 import Products from "./components/pages/productsPage/productsPage.jsx";
@@ -16,19 +16,17 @@ import PurchaseOrders from "./components/pages/purchaseOrdersPage.jsx";
 import Reports from "./components/pages/reportsPage.jsx";
 import Warehouse from "./components/pages/warehousePage.jsx";
 import SignInPage from "./components/pages/SignInPage/SignInPage.jsx";
+import Account from "./components/pages/Setting/account/account.jsx";
+import SettingModal from "./components/pages/Setting/SettingModal/SettingModal.jsx";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const ROUTE_TITLES = {
   "/": "Authentication",
   "/dashboard": "Dashboard",
   "/customers": "Customers",
   "/employees": "Employees",
+  "/employees/add": "Add Employee",
   "/inventory": "Inventory",
   "/orders": "Orders",
   "/products": "Products",
@@ -38,11 +36,6 @@ const ROUTE_TITLES = {
   "/account": "Account",
   "/password": "Password",
 };
-import { useState } from "react";
-import SettingModal from "./components/pages/Setting/SettingModal/SettingModal.jsx";
-import Account from "./components/pages/Setting/account/account.jsx";
-
-//pages and components
 
 function App() {
   const isOpen = useSelector((state) => state.modal.isOpen);
@@ -62,7 +55,8 @@ function App() {
               <Routes>
                 <Route path="/dashboard" element={<DashBoard />} />
                 <Route path="/customers" element={<Customers />} />
-                <Route path="/employees" element={<Employees />} />
+                <Route path="/employees" element={<Employee />} />
+                <Route path="/employees/add" element={<AddEmployee />} /> */
                 <Route path="/inventory" element={<Inventory />} />
                 <Route path="/orders" element={<Orders />} />
                 <Route path="/products" element={<Products />} />
