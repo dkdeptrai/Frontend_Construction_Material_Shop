07af297a1c38e7e3b26d90b049ad5fbfaa87bbeb
Gia Huy <21520909@gm.uit.edu.vn>
2023-12-21 19:16:28 +0700

    ft: add customer to database and check customer details

--- src/components/pages/Customer/InfoCustomer/CustomerInformationPage.jsx
+++ src/components/pages/Customer/InfoCustomer/CustomerInformationPage.jsx
@@ -1,70 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 //pages and components
 import BackButton from "../../../layouts/backButton/backButton";
 import InputComponent from "../../../InputComponent/InputComponent";
-import Table from "../../../core/table/table";
+
 
 function CustomerInformationPage(props) {
-  const columns = [
-    { field: "id", headerName: "ID", flex: 0.2 },
-    { field: "purchaseDate", headerName: "Purchase Date", flex: 0.5 },
-    { field: "numberOfItems", headerName: "Number Of Items", flex: 0.2 },
-    { field: "totalValue", headerName: "Total Value", flex: 0.2 },
-  ];
+  const { id } = useParams();
+  const [customer, setCustomer] = useState();
 
-  const rows = [
-    {
-      id: 1,
-      purchaseDate: "01/01/2021",
-      numberOfItems: "2",
-      totalValue: "5000",
-    },
-    {
-      id: 2,
-      purchaseDate: "01/01/2021",
-      numberOfItems: "2",
-      totalValue: "5000",
-    },
-    {
-      id: 3,
-      purchaseDate: "01/01/2021",
-      numberOfItems: "2",
-      totalValue: "5000",
-    },
-    {
-      id: 4,
-      purchaseDate: "01/01/2021",
-      numberOfItems: "2",
-      totalValue: "5000",
-    },
-    {
-      id: 5,
-      purchaseDate: "01/01/2021",
-      numberOfItems: "2",
-      totalValue: "5000",
-    },
-    {
-      id: 6,
-      purchaseDate: "01/01/2021",
-      numberOfItems: "2",
-      totalValue: "5000",
-    },
-  ];
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/customers/" + id, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    })
+      .then((respone) => respone.json())
+      .then((data) => {
+        const newCustomer = {
+          name: data.name,
+          phone: data.phone,
+          address: data.contactAddress,
+          dateOfBirth: data.dateOfBirth,
+          tax: data.taxCode || "",
+          debt: 0,
+        };
+        setCustomer(newCustomer);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
-  return <div>
-    <BackButton content="Customer Information" />
-    <form>
-      <InputComponent label="Name" type="text" />
-      <InputComponent label="Phone" type="tel" />
-      <InputComponent label="Address" type="text" />
-      <InputComponent label="Date of birth" type="date" />
-      <InputComponent label="Tax" type="text" />
-      <InputComponent label="Debt" type="number" />
-      <label>Old orders:</label>
-
-    </form>
-  </div>;
+  return (
+    <div>
+      <BackButton content="Customer Information" />
+      <form>
+        <InputComponent label="Name" type="text" value={customer?.name} />
+        <InputComponent label="Phone" type="tel" value={customer?.phone} />
+        <InputComponent label="Address" type="text" value={customer?.address} />
+        <InputComponent
+          label="Date of birth"
+          type="date"
+          value={customer?.dateOfBirth}
+        />
+        <InputComponent label="Tax" type="text" value={customer?.tax} />
+        <InputComponent label="Debt" type="number" value={customer?.debt} />
+        <label>Old orders:</label>
+      </form>
+    </div>
+  );
 }
 
 export default CustomerInformationPage;
