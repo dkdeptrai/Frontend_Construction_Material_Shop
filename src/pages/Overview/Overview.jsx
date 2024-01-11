df072693bba5d565ca0242fa8a03f9d859b45305
khoadz <khoa.soft.e@gmail.com>
2024-01-11 11:09:14 +0700

    feat: added orders graph and valuable customer for overview

--- src/pages/Overview/Overview.jsx
+++ src/pages/Overview/Overview.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import {
+  PieChart,
+  Pie,
+  Cell,
   LineChart,
   Line,
   XAxis,
@@ -12,11 +15,13 @@ import {
   AreaChart,
   Label,
 } from "recharts";
+
+import ValuableCustomerComponent from "../../components/ValuableCustomerComponent/valuableCustomerComponent";
+
 import "./Overview.css";
-import { area, lab } from "d3";
 
 function Overview() {
-  const data = [
+  const revenueData = [
     {
       month: "Jan",
       revenue: 900,
@@ -66,13 +71,101 @@ function Overview() {
       revenue: 300,
     },
   ];
+  const orderData = [
+    {
+      name: "Pending",
+      value: 300,
+      fill: "#deefff",
+    },
+    {
+      name: "Delivered",
+      value: 500,
+      fill: "#e1ffe7",
+    },
+    {
+      name: "Cancelled",
+      value: 700,
+      fill: "#ffdfdf",
+    },
+    {
+      name: "Delivering",
+      value: 200,
+      fill: "#fff4d8",
+    },
+  ];
+
+  const customerData = [
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+    {
+      name: "John",
+      phone: "1234567890",
+      orderCount: 5,
+      totalSpent: 500,
+      img: "https://via.placeholder.com/150",
+    },
+  ];
 
   return (
     <>
       <div className="overviewContainer">
         <div className="monthlyRevenue">
           <ResponsiveContainer>
-            <AreaChart width={500} height={400} data={data}>
+            <AreaChart width={500} height={400} data={revenueData}>
               <XAxis dataKey="month"></XAxis>
               <YAxis />
               <Tooltip />
@@ -85,9 +178,34 @@ function Overview() {
             </AreaChart>
           </ResponsiveContainer>
         </div>
-        <div className="ordersStatus">ordersStatus</div>
+        <div className="ordersStatus">
+          <ResponsiveContainer className="pieChartContainer">
+            <PieChart>
+              <Pie
+                data={orderData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                innerRadius={50}
+                outerRadius={90}
+                dataKey="value"
+              ></Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+          <div className="orderLabels">
+            <div className="processingLabel">Processing</div>
+            <div className="deliveringLabel">Delivering</div>
+            <div className="cancelledLabel">Cancelled</div>
+            <div className="deliveredLabel">Delivered</div>
+          </div>
+        </div>
         <div className="topSelling">topSelling</div>
-        <div className="valuableCustomers">valuableCustomers</div>
+        <div className="valuableCustomers">
+          {customerData.map((customer) => (
+            <ValuableCustomerComponent customer={customer} />
+          ))}
+        </div>
       </div>
     </>
   );
