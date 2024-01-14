54ecfcd5a5e94215950150b4a32cc5665c70c3fa
Gia Huy <21520909@gm.uit.edu.vn>
2024-01-15 01:24:40 +0700

    reformat price

--- src/pages/PurchaseOrders/InfoPurchaseOrder/InfoPurchaseOrder.jsx
+++ src/pages/PurchaseOrders/InfoPurchaseOrder/InfoPurchaseOrder.jsx
@@ -98,13 +98,18 @@ const InfoPurchaseOrder = () => {
         </div>
       ),
     },
-    { headerName: "Price/Unit", field: "unitPrice", flex: 0.4 },
+    {
+      headerName: "Price/Unit",
+      field: "unitPrice",
+      flex: 0.4,
+      valueGetter: (params) => params.value.toFixed(2) + " $",
+    },
     { headerName: "Amount", field: "amount", flex: 0.4 },
     {
       headerName: "Total",
       field: "total",
       flex: 0.4,
-      valueGetter: (params) => Math.floor(params.value) + " $",
+      valueGetter: (params) => params.value.toFixed(2) + " $",
     },
   ];
 
@@ -124,7 +129,9 @@ const InfoPurchaseOrder = () => {
       <InformationLine label="Discounts:" content={discount + "%"} />
       <InformationLine
         label="Total:"
-        content={<span style={{ color: "red" }}>{Math.floor(total)} $</span>}
+        content={
+          <span style={{ color: "red" }}>{total.toFixed(2)} $</span>
+        }
       />
 
       <div className="order-state-line">
